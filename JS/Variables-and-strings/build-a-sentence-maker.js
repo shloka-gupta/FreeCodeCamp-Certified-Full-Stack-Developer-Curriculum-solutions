e01f55e0fbaec30405ae8ac5dba2f816ee01bb26
shloka-gupta <shlokaprincess101@gmail.com>
2025-04-02 00:21:53 -0400

    JS

---
+++ JS/Variables-and-strings/build-a-sentence-maker.js
@@ -0,0 +1,21 @@
+let adjective = "silly";
+let noun = "robot";
+let verb = "danced";
+let place = "library";
+let adjective2 = "sparkly";
+let noun2 = "pineapple";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "brave";
+noun = "knight";
+verb = "sang";
+place = "castle";
+adjective2 = "golden";
+noun2 = "sword";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: "+secondStory);
---
+++ JS/Variables-and-strings/lab-javascript-trivia-bot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Claude";
+let botLocation = "Meta Verse";
+let favoriteLanguage = "English";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + "." );
+
+let codingFact = favoriteLanguage + "comes from Latin";
+
+console.log(codingFact);
+
+codingFact =  "The word set has the highest number of different meanings of any word in" + favoriteLanguage;
+
+console.log(codingFact);
+codingFact =  "The word set has 430 different senses in " + favoriteLanguage;
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation +".");
